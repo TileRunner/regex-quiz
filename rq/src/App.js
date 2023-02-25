4e604ed94230f2dc689568624412e5a2d921df39
TileRunner <justchrissykes@gmail.com>
2023-02-25 15:32:15 -0500

    Read json and show data
    Read data as json file and show questions and answers, to show that the data is read ok

--- rq/src/App.js
+++ rq/src/App.js
@@ -1,22 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const getData=()=>{
+    fetch('data.json'
+    ,{
+      headers : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }
+    )
+      .then(function(response){
+        return response.json();
+      })
+      .then(function(myJson) {
+        setData(myJson);
+      });
+  }
+  useEffect(()=>{
+    getData();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>Regex Quiz</h1>
+        <h2>This site is under construction</h2>
+        {data && data.length && data.map((item,index) =>
+        <div key={`item${index}`}>
+          <p>Regular expression {index+1}: <span className='rex'>{item.question}</span></p>
+          <p>Answers:
+            {item.answers && item.answers.length && item.answers.map((answer,answerindex) =>
+            <p className='answer' key={`item${index}answer${answerindex}`}>{answerindex+1}: {answer} </p>)}
+          </p>
+        </div>)}
       </header>
     </div>
   );
