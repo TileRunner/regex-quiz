ed062761dc13178aa21dbc42be0e4e3948e366c4
TileRunner <justchrissykes@gmail.com>
2023-02-26 12:48:09 -0500

    Guesses, answers, help, results
    You can take and retake the quiz and it will show what you missed or got wrong. It does not check if guesses are regex matches of the question yet. It does not allow guess removal yet, so typos are an annoyance.

--- rq/src/App.js
+++ rq/src/App.js
@@ -1,39 +1,91 @@
 import './App.css';
 import {useState, useEffect} from 'react';
+import InputWord from './inputWord';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   const [data, setData] = useState([]);
-  const getData=()=>{
-    fetch('data.json'
+  const [done, setDone] = useState(false);
+
+  // Get the data from a text file in the public folder
+  const getDataTxt=()=>{
+    fetch('RegexQuiz.txt'
     ,{
       headers : {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        'Content-Type': 'application/text',
+        'Accept': 'application/text'
       }
     }
     )
       .then(function(response){
-        return response.json();
+        return response.text();
       })
-      .then(function(myJson) {
-        setData(myJson);
+      .then(function(mytext) {
+        let tabpattern = /\t/; // tab character search
+        let question = "";
+        let answers = [];
+        let quiz = []; // array of {question, answers[], guesses[]}
+        let lines = mytext.split(/\r?\n/); // split file into array of lines
+        lines.forEach(line => {
+          if (line.length && line.search(tabpattern) < 0) {
+            // new question
+            if (question.length) {
+              // finish previous question
+              quiz.push({question: question, answers: answers, guesses: []}); // no guesses yet
+              answers = [];
+            }
+            question = line;
+          } else if (line.length && line.search(tabpattern) === 0) {
+            // answer
+            let answer = line.substring(1);
+            answers.push(answer);
+          }
+        });
+        // finish previous question
+        quiz.push({question: question, answers: answers, guesses: []}); // no guesses yet
+        setData(quiz);
+        setDone(false);
       });
   }
+
   useEffect(()=>{
-    getData();
+    getDataTxt();
   },[])
+  async function submitGuess(questionIndex, guess) {
+    let guesses = data[questionIndex].guesses;
+    let testword = guess.toUpperCase();
+    if (testword && guesses.indexOf(testword) < 0) {
+      let newdata = JSON.parse(JSON.stringify(data));
+      newdata[questionIndex].guesses.push(testword);
+      setData(newdata);
+    }
+  }
   return (
     <div>
       <header>
         <h1>Regex Quiz</h1>
         <h2>This site is under construction</h2>
+        {!done && <Button onClick={() => {setDone(true);}}>Lock in your guesses</Button>}
+        {done && <Button onClick={() => {getDataTxt();}}>Restart the quiz</Button>}
         {data && data.length && data.map((item,index) =>
         <div key={`item${index}`}>
-          <p>Regular expression {index+1}: <span className='rex'>{item.question}</span></p>
-          <p>Answers:
-            {item.answers && item.answers.length && item.answers.map((answer,answerindex) =>
-            <p className='answer' key={`item${index}answer${answerindex}`}>{answerindex+1}: {answer} </p>)}
-          </p>
+          <div className='question'>Regular expression {index+1}: <span className='rex'>{item.question}</span></div>
+          <div>Guesses:
+            <InputWord key={`item${index}guessinput`}
+              handleSubmit={submitGuess}
+              questionIndex={index}
+              question={item.question}/>
+            {item.guesses && item.guesses.map((guess,guessindex) =>
+              <p className='guess' key={`item${index}guess${guessindex}`}>{guessindex+1}: {guess}
+              {done && item.answers.indexOf(guess) < 0 && <span className='wrong'>(wrong)</span>}
+              </p>)}
+          </div>
+          {done && <div>Answers:
+            {item.answers && item.answers.map((answer,answerindex) =>
+              <p className='answer' key={`item${index}answer${answerindex}`}>{answerindex+1}: {answer}
+              {item.guesses.indexOf(answer) < 0 && <span className='missed'>(missed)</span>}
+              </p>)}
+          </div>}
         </div>)}
       </header>
     </div>
---
+++ rq/src/inputWord.js
@@ -0,0 +1,101 @@
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
+const InputWord = ({handleSubmit, questionIndex, question}) => {
+    const [myword, setMyword] = useState('');
+    const [showHelp, setShowHelp] = useState(false);
+    const handleShowHelp = () => setShowHelp(true);
+    const handleCloseHelp = () => setShowHelp(false);
+    const [errorMessage, setErrorMessage] = useState('');
+    const handleClearErrorMessage = () => setErrorMessage('');
+
+    async function mysubmit(event) {
+        event.preventDefault();
+        handleSubmit(questionIndex, myword);
+    }
+
+    return (<div>
+    <Modal
+    show={errorMessage !== ''}
+    onHide={handleClearErrorMessage}
+    animation={false}
+    centered
+    backdrop='static'
+    >
+        <Modal.Header closeButton>
+            <Modal.Title>Attention</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            {errorMessage}
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="secondary" onClick={handleClearErrorMessage}>
+                Close
+            </Button>
+        </Modal.Footer>
+    </Modal>
+    <Offcanvas show={showHelp} onHide={handleCloseHelp} placement='end'>
+        <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Help</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+            <p>Enter a valid word that matches the regular expression: {question}</p>
+            <p>The table below shows how regular expressions work.</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Syntax</th>
+                        <th>Meaning</th>
+                        <th>Example</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>?</td>
+                        <td>The previous item is optional</td>
+                        <td>CAR?T is matched by CAT and CART</td>
+                    </tr>
+                    <tr>
+                        <td>[ABC]</td>
+                        <td>One of the letters inside the square brackets appears next</td>
+                        <td>C[AOU]B is matched by CAB, COB, and CUB</td>
+                    </tr>
+                    <tr>
+                        <td>(A|B|C)</td>
+                        <td>The next sequence of letters is one of the pipe delimited values</td>
+                        <td>C(A|O|UR)B is matched by CAB, COB, and CURB</td>
+                    </tr>
+                    <tr>
+                        <td>.</td>
+                        <td>Any letter</td>
+                        <td>The valid answers for C.B would be CAB, COB, and CUB.</td>
+                    </tr>
+                    <tr>
+                        <td>&#123;x,y&#125;</td>
+                        <td>The previous item occurs x to y times</td>
+                        <td>HM&#123;1,3&#125; is matched by HM, HMM, and HMMM</td>
+                    </tr>
+                </tbody>
+            </table>
+        </Offcanvas.Body>
+    </Offcanvas>
+    <Form onSubmit={mysubmit}>
+        <InputGroup>
+            <Button variant="outline-secondary" onClick={() => {setMyword('')}}>Clear</Button>
+            <Form.Control
+            type="text"
+            value={myword}
+            onChange={e => { setMyword(e.target.value); } }
+            placeholder="Your word..."
+            />
+            <Button variant="primary" type='submit'>Submit</Button>
+            <Button variant="info" onClick={handleShowHelp}>Help</Button>
+        </InputGroup>
+    </Form>
+    </div>);
+}
+
+export default InputWord;
