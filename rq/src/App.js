979313eaca23f217b2c21c22808efba5020f3cd4
TileRunner <justchrissykes@gmail.com>
2023-03-04 12:57:36 -0500

    Split quiz into multiple (not all done yet)
    Multiple quizzes rather than one big one.

--- rq/src/App.js
+++ rq/src/App.js
@@ -1,147 +1,63 @@
 import './App.css';
 import {useState, useEffect} from 'react';
-import InputWord from './inputWord';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import TakeQuiz from './components/takeQuiz';
+import { Button, Table } from 'react-bootstrap';
 
 function App() {
-  const [data, setData] = useState([]);
-  const [done, setDone] = useState(false);
-  const [currentIndex, setCurrentIndex] = useState(-1);
-  const [results, setResults] = useState({correct: 0, wrong: 0, missed: 0, points: 0});
+  const [qlist, setQlist] = useState([]);
+  const [qindex, setQindex] = useState(-1);
 
-  // Get the data from a text file in the public folder
-  const getDataTxt=()=>{
-    fetch('RegexQuiz.txt'
+  const getQuizzes=()=>{
+    fetch('quizzes/qlist.json'
     ,{
       headers : {
-        'Content-Type': 'application/text',
-        'Accept': 'application/text'
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
       }
     }
     )
       .then(function(response){
-        return response.text();
+        return response.json();
       })
-      .then(function(mytext) {
-        let tabpattern = /\t/; // tab character search
-        let question = "";
-        let answers = [];
-        let quiz = []; // array of {question, answers[], guesses[]}
-        let lines = mytext.split(/\r?\n/); // split file into array of lines
-        lines.forEach(line => {
-          if (line.length > 1 && line.search(tabpattern) < 0) {
-            // new question
-            if (question.length) {
-              // finish previous question
-              answers.sort();
-              quiz.push({question: question, answers: answers, guesses: []}); // no guesses yet
-              answers = [];
-            }
-            question = line;
-          } else if (line.length > 1 && line.search(tabpattern) === 0) {
-            // answer
-            let answer = line.substring(1);
-            answers.push(answer);
-          }
-        });
-        // finish previous question
-        answers.sort();
-        quiz.push({question: question, answers: answers, guesses: []}); // no guesses yet
-        setData(quiz);
-        setCurrentIndex(0);
-        setDone(false);
-        setResults({correct: 0, wrong: 0, missed: 0, points: 0});
+      .then(function(myJson) {
+        setQlist(myJson);
       });
   }
 
   useEffect(()=>{
-    getDataTxt();
+    getQuizzes();
   },[])
-  async function submitGuess(questionIndex, guess) {
-    let guesses = data[questionIndex].guesses;
-    let testword = guess.toUpperCase();
-    if (testword && guesses.indexOf(testword) < 0) {
-      let newdata = JSON.parse(JSON.stringify(data));
-      newdata[questionIndex].guesses.push(testword);
-      newdata[questionIndex].guesses.sort();
-      setData(newdata);
-    }
-  }
-  function finishQuiz() {
-    let correct = 0;
-    let wrong = 0;
-    let missed = 0;
-    let points = 0;
-    data.forEach((item) => {
-      item.guesses.forEach((guess) => {
-        if (item.answers.indexOf(guess) < 0) {
-          wrong++;
-          points = points - 2;
-        } else {
-          correct++;
-          points++;
-        }
-      });
-      item.answers.forEach((answer) => {
-        if (item.guesses.indexOf(answer) < 0) {
-          missed++;
-        }
-      });
-    });
-    setResults({correct: correct, wrong: wrong, missed: missed, points: points});
-    setDone(true);
-  }
   return (
     <div>
       <header>
-        <h1>Regex Quiz</h1>
-        {!done && <Button onClick={() => {finishQuiz();}}>Lock in your guesses</Button>}
-        {done && <div>
-          <h2>Correct: {results.correct}, Wrong: {results.wrong}, Missed: {results.missed}, Total Points: {results.points}</h2>
-          <Button onClick={() => {getDataTxt();}}>Restart the quiz</Button>
+        <h1>Quiz List</h1>
+        {qlist && qlist.length && <div>
+          <Table striped bordered hover variant="dark">
+            <thead>
+              <tr>
+                <th>Description</th>
+                <th>Lexicon</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {qlist.filter((q,qi) => qi === qindex || qindex < 0).map((q,qi) =>
+                <tr key={`quizlistitem${q.id}`}>
+                  <td>{q.desc}</td>
+                  <td>{q.lexicon}</td>
+                  <td>
+                    {qindex === q.id ?
+                    <Button onClick={() => {setQindex(-1);}}>Quit this quiz</Button>
+                    :
+                    <Button onClick={() => {setQindex(q.id);}}>Take this quiz</Button>
+                    }
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+          {qindex > -1 && <TakeQuiz filename={`quizzes/${qlist[qindex].filename}`}></TakeQuiz>}
         </div>}
-        {data && data.length && data.map((item,index) =>
-          <div key={`item${index}`}>
-            {done ? <div>
-              <div className='question'>Regular expression {index+1}: <span className='rex'>{item.question}</span></div>
-              <Row>
-                <Col>
-                  <div>
-                    <h2>Guesses</h2>
-                    {item.guesses && item.guesses.map((guess,guessindex) =>
-                      <p className='guess' key={`item${index}guess${guessindex}`}>{guessindex+1}: {guess}
-                      {done && item.answers.indexOf(guess) < 0 && <span className='wrong'>(wrong)</span>}
-                      </p>)}
-                  </div>
-                </Col>
-                <Col>
-                  <div>
-                    <h2>Answers</h2>
-                    {item.answers && item.answers.map((answer,answerindex) =>
-                      <p className='answer' key={`item${index}answer${answerindex}`}>{answerindex+1}: {answer}
-                      {item.guesses.indexOf(answer) < 0 && <span className='missed'>(missed)</span>}
-                      </p>)}
-                  </div>
-                </Col>
-              </Row>
-            </div> : index === currentIndex && <div>
-              <div className='question'>Regular expression {index+1}: <span className='rex'>{item.question}</span></div>
-              <div>Guesses:
-                <InputWord key={`item${index}guessinput`}
-                  handleSubmit={submitGuess}
-                  questionIndex={index}
-                  question={item.question}/>
-                {item.guesses && item.guesses.map((guess,guessindex) =>
-                  <p className='guess' key={`item${index}guess${guessindex}`}>{guessindex+1}: {guess}
-                  {done && item.answers.indexOf(guess) < 0 && <span className='wrong'>(wrong)</span>}
-                  </p>)}
-                {currentIndex > 0 && <Button onClick={() => setCurrentIndex(currentIndex-1)}>Previous</Button>}
-                {currentIndex + 1 < data.length && <Button onClick={() => setCurrentIndex(currentIndex+1)}>Next</Button>}
-              </div>
-            </div>}
-          </div>)}
       </header>
     </div>
   );
---
+++ rq/src/components/takeQuiz.js
@@ -0,0 +1,149 @@
+import '../App.css';
+import {useState, useEffect} from 'react';
+import InputWord from './inputWord';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+const TakeQuiz=({filename}) => {
+    const [data, setData] = useState([]);
+    const [done, setDone] = useState(false);
+    const [currentIndex, setCurrentIndex] = useState(-1);
+    const [results, setResults] = useState({correct: 0, wrong: 0, missed: 0, points: 0});
+
+    // Get the data from a text file in the public folder
+    const getDataTxt=(quizpath)=>{
+        fetch(quizpath
+        ,{
+            headers : {
+            'Content-Type': 'application/text',
+            'Accept': 'application/text'
+            }
+        }
+        )
+        .then(function(response){
+            return response.text();
+        })
+        .then(function(mytext) {
+            let tabpattern = /\t/; // tab character search
+            let question = "";
+            let answers = [];
+            let quiz = []; // array of {question, answers[], guesses[]}
+            let lines = mytext.split(/\r?\n/); // split file into array of lines
+            lines.forEach(line => {
+                if (line.length > 1 && line.search(tabpattern) < 0) {
+                // new question
+                if (question.length) {
+                    // finish previous question
+                    answers.sort();
+                    quiz.push({question: question, answers: answers, guesses: []}); // no guesses yet
+                    answers = [];
+                }
+                question = line;
+                } else if (line.length > 1 && line.search(tabpattern) === 0) {
+                // answer
+                let answer = line.substring(1);
+                answers.push(answer);
+                }
+            });
+            // finish previous question
+            answers.sort();
+            quiz.push({question: question, answers: answers, guesses: []}); // no guesses yet
+            setData(quiz);
+            setCurrentIndex(0);
+            setDone(false);
+            setResults({correct: 0, wrong: 0, missed: 0, points: 0});
+        });
+    }
+
+    useEffect(()=>{
+        getDataTxt(filename);
+    },[filename])
+
+    async function submitGuess(questionIndex, guess) {
+        let guesses = data[questionIndex].guesses;
+        let testword = guess.toUpperCase();
+        if (testword && guesses.indexOf(testword) < 0) {
+            let newdata = JSON.parse(JSON.stringify(data));
+            newdata[questionIndex].guesses.push(testword);
+            newdata[questionIndex].guesses.sort();
+            setData(newdata);
+        }
+    }
+
+    function finishQuiz() {
+        let correct = 0;
+        let wrong = 0;
+        let missed = 0;
+        let points = 0;
+        data.forEach((item) => {
+            item.guesses.forEach((guess) => {
+            if (item.answers.indexOf(guess) < 0) {
+                wrong++;
+                points = points - 2;
+            } else {
+                correct++;
+                points++;
+            }
+            });
+            item.answers.forEach((answer) => {
+            if (item.guesses.indexOf(answer) < 0) {
+                missed++;
+            }
+            });
+        });
+        setResults({correct: correct, wrong: wrong, missed: missed, points: points});
+        setDone(true);
+    }
+
+    return ( <div>
+        <h1>Regex Quiz</h1>
+        {!done && <Button onClick={() => {finishQuiz();}}>Lock in your guesses</Button>}
+        {done && <div>
+            <h2>Correct: {results.correct}, Wrong: {results.wrong}, Missed: {results.missed}, Total Points: {results.points}</h2>
+            <Button onClick={() => {getDataTxt();}}>Restart the quiz</Button>
+        </div>}
+        {data && data.length && data.map((item,index) =>
+            <div key={`item${index}`}>
+            {done ? <div>
+                <div className='question'>Regular expression {index+1}: <span className='rex'>{item.question}</span></div>
+                <Row>
+                <Col>
+                    <div>
+                    <h2>Guesses</h2>
+                    {item.guesses && item.guesses.map((guess,guessindex) =>
+                        <p className='guess' key={`item${index}guess${guessindex}`}>{guessindex+1}: {guess}
+                        {done && item.answers.indexOf(guess) < 0 && <span className='wrong'>(wrong)</span>}
+                        </p>)}
+                    </div>
+                </Col>
+                <Col>
+                    <div>
+                    <h2>Answers</h2>
+                    {item.answers && item.answers.map((answer,answerindex) =>
+                        <p className='answer' key={`item${index}answer${answerindex}`}>{answerindex+1}: {answer}
+                        {item.guesses.indexOf(answer) < 0 && <span className='missed'>(missed)</span>}
+                        </p>)}
+                    </div>
+                </Col>
+                </Row>
+            </div> : index === currentIndex && <div>
+                <div className='question'>Regular expression {index+1}: <span className='rex'>{item.question}</span></div>
+                <div>Guesses:
+                <InputWord key={`item${index}guessinput`}
+                    handleSubmit={submitGuess}
+                    questionIndex={index}
+                    question={item.question}/>
+                {item.guesses && item.guesses.map((guess,guessindex) =>
+                    <p className='guess' key={`item${index}guess${guessindex}`}>{guessindex+1}: {guess}
+                    {done && item.answers.indexOf(guess) < 0 && <span className='wrong'>(wrong)</span>}
+                    </p>)}
+                {currentIndex > 0 && <Button onClick={() => setCurrentIndex(currentIndex-1)}>Previous</Button>}
+                {currentIndex + 1 < data.length && <Button onClick={() => setCurrentIndex(currentIndex+1)}>Next</Button>}
+                </div>
+            </div>}
+        </div>)}
+    </div>);
+}
+
+export default TakeQuiz;
