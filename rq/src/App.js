d271efb75ead8d61d462033340977378f3dc90af
TileRunner <justchrissykes@gmail.com>
2023-08-27 09:01:49 -0400

    Update App.js
    Home 'page' blurb added.

--- rq/src/App.js
+++ rq/src/App.js
@@ -18,6 +18,45 @@ const MODE_MINEFIELD = 'Minefields';
 const MODE_POTLUCK = 'Potlucks';
 const MODE_DOUBLESIXES = 'Double Sixes';
 
+const Home = <div>
+  <a href="http://www.scrabbleplayers.org"><img border="0" src="http://www.scrabbleplayers.org/pix/logo-only-160px.png" alt="[NASPA Logo]" /></a>
+  <span className='ackNASPA'>NWL20 used with permission from NASPA</span>
+  <div className='p-2 bg-secondary text-white'>
+    <p>The original concept for this site was to use it for trivia contests at long tournaments.
+      I wanted to use the Regular Expressions as the questions, and individuals or teams would
+      get one point per correct word, and lose one point per phony. The Regex Quizzes mode best reflects this original intention.
+    </p>
+    <p>
+      The Ladders mode is the first offshoot. It is a more brutal format in that you have to reclimb the ladder
+      from the bottom rung as soon as you make a mistake. The idea is to keep trying until you successfully
+      climb the ladder, hopefully helping you cement the words as you go.
+    </p>
+    <p>
+      The Minefields idea is to help in the fight against plausible phonies.
+      Each quiz shows you pairs of words, one phony and one valid, and you click on the valid words.
+      If you click on the phony, the minefield explodes (no fancy graphics).
+    </p>
+    <p>
+      The Potluck quizzes are similar to minefields. It shows one candidate word at a time,
+      and you have to decide whether it is valid or phony. Any mistakes and the quiz ends.
+      It will show all the valid words wen the quiz ends.
+      There may be multiple valid words with the same letters, but only one phony per alphagram.
+      The current set of quizzes reflects all the mistakes I am making this year (2023) as I revise
+      all 2-8 letter words in a year.
+    </p>
+    <p>
+      The Double Six quizzes are challenging puzzles (for me at least), where you have to use the 12
+      letters shown and split them into two six letter words.
+    </p>
+  </div>
+  <div className='p-2 bg-danger text-white'>
+    <p>If you are a developer and would like to help with this site, you can go to my&nbsp;
+      <a className='' href="https://tilerunner.github.io/">Home</a> site to find my contact info.
+      It is written in React.
+    </p>
+  </div>
+</div>;
+
 function App() {
   const [mode, setMode] = useState(MODE_HOME);
   const [llist, setLlist] = useState([]);
@@ -88,10 +127,7 @@ function App() {
                 </div>
             }
           </div>
-        {mode === MODE_HOME && <div>
-          <a href="http://www.scrabbleplayers.org"><img border="0" src="http://www.scrabbleplayers.org/pix/logo-only-160px.png" alt="[NASPA Logo]"/></a>
-          <span className='ackNASPA'>NWL20 used with permission from NASPA</span>
-        </div>}
+        {mode === MODE_HOME && Home}
         {mode === MODE_REGEX && ShowRegexMode}
         {mode === MODE_LADDER && <Ladder/>}
         {mode === MODE_MINEFIELD && <Minefield mlist={mlist} mindex={mindex} setMindex={setMindex} mdesc={mdesc} setMdesc={setMdesc} />}
